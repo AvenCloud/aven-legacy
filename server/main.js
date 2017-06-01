9e03ff55e8db5730e8efc202b5be9c736b750eba
Eric Vicenti <ericvicenti@gmail.com>
2017-06-01 08:06:33 -0700

    wut

--- server/main.js
+++ server/main.js
@@ -1,12 +1,39 @@
 var express = require("express");
 var app = express();
 
-app.set("port", process.env.PORT || 5000);
+var fs = require("fs");
+var secrets;
+
+try {
+  secrets = fs.readFileSync("secrets.json", { encoding: "utf8" });
+  secrets = JSON.parse(secrets);
+} catch (e) {
+  secrets = new Buffer(process.env.AVEN_SECRETS, "base64");
+  secrets = JSON.parse(secrets.toString());
+}
+
+console.log(secrets);
+
+const CONFIG = {
+  env: process.env.NODE_ENV,
+  port: process.env.PORT || 5000,
+  secrets: secrets
+};
 
 app.get("/", function(req, res) {
-  res.send("Hey there boyo!");
+  res.send("Coming Soon!");
 });
 
-app.listen(app.get("port"), function() {
-  console.log("Node app is running on port", app.get("port"));
+app.get("/debug", function(req, res) {
+  res.send(
+    JSON.stringify({
+      env: CONFIG.env,
+      port: CONFIG.port,
+      secrets: Object.keys(CONFIG.secrets)
+    })
+  );
+});
+
+app.listen(CONFIG.port, function() {
+  console.log("Node app is running on port", CONFIG.port);
 });
